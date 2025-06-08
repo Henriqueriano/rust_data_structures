f0e8b20a5e30a8dfa2f335782f4389f5324468ab
Daniel Henrique <daniel.neves0021@gmail.com>
2025-06-08 15:39:46 -0300

    fix: revert commit

---
+++ queue/target/debug/deps/queue.d
@@ -0,0 +1,5 @@
+C:\Users\danie\Desktop\rust_data_structures\queue\target\debug\deps\queue.exe: src\main.rs
+
+C:\Users\danie\Desktop\rust_data_structures\queue\target\debug\deps\queue.d: src\main.rs
+
+src\main.rs:
---
+++ queue/target/debug/queue.d
@@ -0,0 +1,1 @@
+C:\Users\danie\Desktop\rust_data_structures\queue\target\debug\queue.exe: C:\Users\danie\Desktop\rust_data_structures\queue\src\main.rs
